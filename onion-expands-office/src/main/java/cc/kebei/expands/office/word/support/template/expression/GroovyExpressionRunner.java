f81fa5ac36f3d16bc04c5d771b1ac102255171f6
柯北 <1209547238@qq.com>
2019-06-21 20:32:32 +0800

    commit 3.0.1 snapshot 修改javadoc

--- onion-expands-office/src/main/java/cc/kebei/expands/office/word/support/template/expression/GroovyExpressionRunner.java
+++ onion-expands-office/src/main/java/cc/kebei/expands/office/word/support/template/expression/GroovyExpressionRunner.java
@@ -23,7 +23,7 @@ import java.util.*;
  * 变量引用:  ${name}
  * 表格：
  * 变量引用:  ${name}
- * 循环:  ${for(data in list)} | ${data.name} |${data.addr} |${/for}  。注意:表达式必须在一行</li>
+ * 循环:  ${for(data in list)} | ${data.name} |${data.addr} |${/for}  。注意:表达式必须在一行
  * Created by 浩 on 2015-12-18 0018.
  */
 public class GroovyExpressionRunner implements ExpressionRunner {
